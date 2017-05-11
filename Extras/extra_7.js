19b03741610e2b48dcea7e08b6bd92b3e5f337e4
Julio Maciel <juketo@gmail.com>
2017-05-10 23:08:59 -0300

    doing chapter 7

---
+++ Extras/extra_7.js
@@ -0,0 +1,5 @@
+// instalar o mocha
+'npm install mocha --save-dev'
+// -dev indica que deve ficar restrito ao ambiente de dev
+// na sequencia, rodar o arquivo de script
+'node node_modules/mocha/bin/mocha'
